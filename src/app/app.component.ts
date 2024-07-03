6a5280fa19cd7ab96a14807f1ce15ea957b61ca1
Tapu-gitname <sahusoumyaranjan2@gmail.com>
2024-07-04 00:46:27 +0530

    added all the configuration so that Django BE and Angular FE both can work in https://127.0.0.1:8000

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 // import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HttpClientModule],
+  providers: [ApiService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-patient-list',
@@ -8,15 +9,29 @@ import { TableModule } from 'primeng/table';
   templateUrl: './patient-list.component.html',
   styleUrl: './patient-list.component.css'
 })
-export class PatientListComponent {
+export class PatientListComponent implements OnInit {
 
-  patients: any[] = [
-    { name: 'John Doe', phone: '123-456-7890', treatment: 'Cleaning' },
-    { name: 'Jane Smith', phone: '987-654-3210', treatment: 'Filling' },
-    { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
-    { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
-    { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
-    { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' }
-  ];
+  patients: any = []
+
+  constructor(private apiService: ApiService) {}
+
+  // patients: any[] = [
+  //   { name: 'John Doe', phone: '123-456-7890', treatment: 'Cleaning' },
+  //   { name: 'Jane Smith', phone: '987-654-3210', treatment: 'Filling' },
+  //   { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
+  //   { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
+  //   { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' },
+  //   { name: 'Sam Wilson', phone: '555-123-4567', treatment: 'Root Canal' }
+  // ];
+  
+  ngOnInit(): void {
+    this.apiService.getPatients().subscribe((data) => {
+      this.patients = data;
+    },
+    (error) => {
+      console.error('Error fetching series', error);
+    });
+  }
+
 
 }
